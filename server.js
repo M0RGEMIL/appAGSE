c9a52b04283c09d96c0acdf015a15e9bde04dd38
Augustin Leconte <augustin.leconte@epitech.eu>
2019-11-06 13:25:50 +0100

    commit connect heroku

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
-//Install express server
-const express = require('express');
-const path = require('path');
+var express = require('express');
 
-const app = express();
+app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/appAGSE'));
+app.use(express.static(__dirname + '/www'));
 
-app.get('/*', function(req,res) {
-
-res.sendFile(path.join(__dirname+'/dist/appAGSE/index.html'));
+// CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.set('port', process.env.PORT || 8080);
+
+app.listen(app.get('port'), function () {
+  // console.log('Express server listening on port ' + app.get('port'));
+});
