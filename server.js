569e9502bb982be30d87269b9ef5819f0ff5c2c7
Augustin Leconte <augustin.leconte@epitech.eu>
2019-11-06 13:53:04 +0100

    add '/' route

--- server.js
+++ server.js
@@ -16,3 +16,7 @@ app.set('port', process.env.PORT || 8080);
 app.listen(app.get('port'), function () {
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+app.get('/', function(req, res){
+   res.redirect('/app');
+});
